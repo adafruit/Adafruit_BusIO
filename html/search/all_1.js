19fb55d6a7bea62fe6f01349a11c097ca4ebced5
Travis CI <travis@travis-ci.org>
2019-10-08 03:26:29 +0000

    Deploy code docs to GitHub Pages Travis build: 39
    Commit: a403ba0dabc17c27f55c4692158abcc47dbbcf7b

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___i2_c_device.html#a14ebb7e821697ed9391ac838a0f29582',1,'Adafruit_I2CDevice::begin()'],['../class_adafruit___s_p_i_device.html#a6aa3c0bebb1d2d58516bce4f2dd1423f',1,'Adafruit_SPIDevice::begin()']]]
+  ['begin',['begin',['../class_adafruit___i2_c_device.html#ada579d107621ac5f5e56f91f94a93be3',1,'Adafruit_I2CDevice::begin()'],['../class_adafruit___s_p_i_device.html#a6aa3c0bebb1d2d58516bce4f2dd1423f',1,'Adafruit_SPIDevice::begin()']]]
 ];
