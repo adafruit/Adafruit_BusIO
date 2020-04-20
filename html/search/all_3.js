4779d4af0417ee5435d399cad0534488e429af48
Doxygen CI <travis@travis-ci.org>
2020-04-20 23:32:27 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['maxbuffersize',['maxBufferSize',['../class_adafruit___i2_c_device.html#a20c6ac28cbaa9e5f63ef6d4687fd4e60',1,'Adafruit_I2CDevice']]]
+  ['maxbuffersize',['maxBufferSize',['../class_adafruit___i2_c_device.html#a29db73c8e4f72923286b4c5c358afc8a',1,'Adafruit_I2CDevice']]]
 ];
