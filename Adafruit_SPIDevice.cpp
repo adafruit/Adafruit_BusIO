a5c631593e7e859fe0fb9f1b2de289bdb37cbf7a
howard-wa9axq <75182246+howard-wa9axq@users.noreply.github.com>
2020-11-28 14:15:01 -0600

    Update Adafruit_SPIDevice.cpp
    The SPI.cpp that the STM32 project uses does not have a transfer method with both a buffer and a length.  Changes to this file uses a transfer method that just needs a single 8 bit character.

--- Adafruit_SPIDevice.cpp
+++ Adafruit_SPIDevice.cpp
@@ -116,7 +116,8 @@ void Adafruit_SPIDevice::transfer(uint8_t *buffer, size_t len) {
 #ifdef SPARK
     _spi->transfer(buffer, buffer, len, NULL);
 #else
-    _spi->transfer(buffer, len);
+    for(i = 0; i < len; i++ )
+      _spi->transfer(buffer[i]);
 #endif
 
     return;
