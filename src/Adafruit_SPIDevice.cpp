65f13b36836080f47d6465da1030edf43e6f8781
James Foster <github@jgfoster.net>
2021-02-02 16:02:00 -0800

    Update to latest from Adafruit; Update to Arduino-CI/action@stable-1.x (#9)
    * Make compatible with Heltec's CubeCell Arduino-core
    
    The functions fast pin-I/O uses are currently missing, so don't define BUSIO_USE_FAST_PINIO. Also, we need typedef for BitOrder, but it refuses to compile without also including Arduino.h, at least under PlatformIO.
    
    * Update Adafruit_SPIDevice.h
    
    * Buffer arguments of write functions are changed to const so that the functions cannot change the content of the buffer. Also, if the I2C device's register address are define using const rather than #define, the functions can accept those too.
    
    * Applied clang formatting.
    
    * Added function description.
    
    * Update Adafruit_SPIDevice.cpp
    
    The SPI.cpp that the STM32 project uses does not have a transfer method with both a buffer and a length.  Changes to this file uses a transfer method that just needs a single 8 bit character.
    
    * Update Adafruit_SPIDevice.cpp
    
    Added code to define 'i'.
    
    * Update Adafruit_SPIDevice.cpp
    
    Adjusted syntax of added 'for' statement
    
    * Update Adafruit_SPIDevice.cpp
    
    * Update Adafruit_SPIDevice.cpp
    
    Changing #ifdef construct to make STM32 a special case.
    
    * Updated #if #elif structure.
    
    Update #if #elif structure to avoid nested #ifdef's
    
    * Removed extra blank line
    
    * Update Adafruit_SPIDevice.cpp
    
    * Revert "Added function description."
    
    This reverts commit 0475316f1e948d91b7b2a31b2761174b26061b12.
    
    * Write functions' description updated that explains why the buffer arguments are const.
    
    * Bump to 1.7.0
    
    * Release memory allocated in constructors
    
    * Edit comment to match other functions.
    
    * Bump to 1.7.1
    
    * Define the meaning of 'type' for BusIO Registers
    
    Addressed issue #19 by updating register consts with suggested descriptions.
    
    Co-Authored-By: jayjlawrence <1862335+jayjlawrence@users.noreply.github.com>
    
    * Bump to 1.7.2
    
    * Remove duplicate definition of destructor.
    
    Co-authored-by: WereCatf <werecatf@gmail.com>
    Co-authored-by: Limor "Ladyada" Fried <limor@ladyada.net>
    Co-authored-by: DESKTOP\fahadmirza <fahadmirza8@gmail.com>
    Co-authored-by: howard-wa9axq <75182246+howard-wa9axq@users.noreply.github.com>
    Co-authored-by: Dylan Herrada <33632497+dherrada@users.noreply.github.com>
    Co-authored-by: Stuart Feichtinger <stuart.feichtinger@gmail.com>
    Co-authored-by: jayjlawrence <1862335+jayjlawrence@users.noreply.github.com>
    Co-authored-by: James Foster <git@jgfoster.net>

--- src/Adafruit_SPIDevice.cpp
+++ src/Adafruit_SPIDevice.cpp
@@ -75,7 +75,9 @@ Adafruit_SPIDevice::Adafruit_SPIDevice(int8_t cspin, int8_t sckpin,
   _spi = NULL;
 }
 
-// Release memory allocated in constructors
+/*!
+ *    @brief  Release memory allocated in constructors
+ */
 Adafruit_SPIDevice::~Adafruit_SPIDevice() {
   if (_spiSetting) {
     delete _spiSetting;
@@ -126,12 +128,15 @@ void Adafruit_SPIDevice::transfer(uint8_t *buffer, size_t len) {
   if (_spi) {
     // hardware SPI is easy
 
-#ifdef SPARK
+#if defined(SPARK)
     _spi->transfer(buffer, buffer, len, NULL);
+#elif defined(STM32)
+    for (size_t i = 0; i < len; i++) {
+      _spi->transfer(buffer[i]);
+    }
 #else
     _spi->transfer(buffer, len);
 #endif
-
     return;
   }
 
