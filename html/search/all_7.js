3e953c7467c9e2ed5fb08443acc672c51b37ea53
Doxygen CI <ci-arduino@invalid>
2021-06-09 10:02:49 +0000

    Deploy docs to GitHub Pages from commit 11d3cbcad1
    Commit: 11d3cbcad1fc6abc66727a760c0486a19446c6f4
    GitHub Actions run: 921280178

--- html/search/all_7.js
+++ html/search/all_7.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['setspeed',['setSpeed',['../class_adafruit___i2_c_device.html#a784c398626837ef911ce7e69b0807be7',1,'Adafruit_I2CDevice']]]
+  ['setaddress',['setAddress',['../class_adafruit___bus_i_o___register.html#a08ddf29ed836c712e2a2f050e1ae3bcd',1,'Adafruit_BusIO_Register']]],
+  ['setaddresswidth',['setAddressWidth',['../class_adafruit___bus_i_o___register.html#a421a7c74ab0faafe243060af1883717a',1,'Adafruit_BusIO_Register']]],
+  ['setspeed',['setSpeed',['../class_adafruit___i2_c_device.html#a784c398626837ef911ce7e69b0807be7',1,'Adafruit_I2CDevice']]],
+  ['setwidth',['setWidth',['../class_adafruit___bus_i_o___register.html#a478ae4ca787304ead47a56994fbdb840',1,'Adafruit_BusIO_Register']]]
 ];
