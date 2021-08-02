2236453fb328dff517b4012dfb667f82483ea2db
Francesco Stefanni <francesco.stefanni@gizeroenergie.it>
2021-08-02 15:39:10 +0200

    Added missing Adafruit_BusIO.h required by arduino-cli

---
+++ Adafruit_BusIO.h
@@ -0,0 +1,9 @@
+#ifndef ADAFRUIT_BUSIO_H
+#define ADAFRUIT_BUSIO_H
+
+#include "Adafruit_BusIO_Register.h"
+#include "Adafruit_I2CDevice.h"
+#include "Adafruit_I2CRegister.h"
+#include "Adafruit_SPIDevice.h"
+
+#endif
