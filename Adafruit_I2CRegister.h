8b0ccda6bdf137ea799e58b526f0f86c8a36c79f
lady ada <limor@ladyada.net>
2021-11-20 01:09:02 -0500

    ckag

--- Adafruit_I2CRegister.h
+++ Adafruit_I2CRegister.h
@@ -1,8 +1,8 @@
 #ifndef _ADAFRUIT_I2C_REGISTER_H_
 #define _ADAFRUIT_I2C_REGISTER_H_
 
-#include <Arduino.h>
 #include <Adafruit_BusIO_Register.h>
+#include <Arduino.h>
 
 typedef Adafruit_BusIO_Register Adafruit_I2CRegister;
 typedef Adafruit_BusIO_RegisterBits Adafruit_I2CRegisterBits;
