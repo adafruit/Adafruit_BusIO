048b631c9f4b6040d3db5c3c5064904ed99d3583
Doxygen CI <ci-arduino@invalid>
2021-11-24 11:13:17 +0000

    Deploy docs to GitHub Pages from commit 5d6fa8dfaf
    Commit: 5d6fa8dfaf9ed2b316309ed14451b8ca45f4ac30
    GitHub Actions run: 1499035760

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['end',['end',['../class_adafruit___i2_c_device.html#afe9d8e8a154d29acaaeb1d00ddd8da08',1,'Adafruit_I2CDevice']]],
   ['endtransaction',['endTransaction',['../class_adafruit___s_p_i_device.html#a65b517b7372df56322b807a08d067f8b',1,'Adafruit_SPIDevice']]]
 ];
