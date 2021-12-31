d9d82500cf7a9fce509aa7e10911aaaaccee7e0a
Doxygen CI <ci-arduino@invalid>
2021-12-31 18:35:29 +0000

    Deploy docs to GitHub Pages from commit 33ff0c4bbc
    Commit: 33ff0c4bbc910929372c7cba0cb1d6c1dd78b271
    GitHub Actions run: 1641454272

--- html/search/all_9.js
+++ html/search/all_9.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['width',['width',['../class_adafruit___bus_i_o___register.html#abf1eaeb0ae836be082a73dae14e7e352',1,'Adafruit_BusIO_Register']]],
   ['write',['write',['../class_adafruit___bus_i_o___register.html#aa418dced5301a1d8c1776a1941c8bc64',1,'Adafruit_BusIO_Register::write(uint8_t *buffer, uint8_t len)'],['../class_adafruit___bus_i_o___register.html#adad13ec14c8548eaeff62609077aa076',1,'Adafruit_BusIO_Register::write(uint32_t value, uint8_t numbytes=0)'],['../class_adafruit___bus_i_o___register_bits.html#a9311f94f1a1d93bc575b06c34ddecd80',1,'Adafruit_BusIO_RegisterBits::write()'],['../class_adafruit___i2_c_device.html#abf8eb0d537eec82ead72670ab9803622',1,'Adafruit_I2CDevice::write()'],['../class_adafruit___s_p_i_device.html#a98bc2e396650883935a5cb432716956a',1,'Adafruit_SPIDevice::write()']]],
+  ['write_5fand_5fread',['write_and_read',['../class_adafruit___s_p_i_device.html#a808c81bc4b9e42d064b9f590a1f6b1bd',1,'Adafruit_SPIDevice']]],
   ['write_5fthen_5fread',['write_then_read',['../class_adafruit___i2_c_device.html#af04af7e2918c873215ce949ce05ebe33',1,'Adafruit_I2CDevice::write_then_read()'],['../class_adafruit___s_p_i_device.html#a8128f7d67b3e0e56717279deb64227e7',1,'Adafruit_SPIDevice::write_then_read()']]]
 ];
