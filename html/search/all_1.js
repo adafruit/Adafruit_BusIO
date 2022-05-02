ecf41d40ee7eb7d3cd46afc383e2f3b24f42fcf2
Doxygen CI <ci-arduino@invalid>
2022-05-02 22:30:26 +0000

    Deploy docs to GitHub Pages from commit bb7c77ad09
    Commit: bb7c77ad093ddbe44e9608f21e2dd37311ad13b6
    GitHub Actions run: 2260793423

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['begin',['begin',['../class_adafruit___i2_c_device.html#ada579d107621ac5f5e56f91f94a93be3',1,'Adafruit_I2CDevice::begin()'],['../class_adafruit___s_p_i_device.html#a6aa3c0bebb1d2d58516bce4f2dd1423f',1,'Adafruit_SPIDevice::begin()']]],
-  ['begintransaction',['beginTransaction',['../class_adafruit___s_p_i_device.html#acde87a3312ddc729c259b1b45fe71d82',1,'Adafruit_SPIDevice']]]
+  ['begintransaction',['beginTransaction',['../class_adafruit___s_p_i_device.html#acde87a3312ddc729c259b1b45fe71d82',1,'Adafruit_SPIDevice']]],
+  ['begintransactionwithassertingcs',['beginTransactionWithAssertingCS',['../class_adafruit___s_p_i_device.html#a11e91bf075cdd77edcec11f4b28c6292',1,'Adafruit_SPIDevice']]]
 ];
--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['end',['end',['../class_adafruit___i2_c_device.html#afe9d8e8a154d29acaaeb1d00ddd8da08',1,'Adafruit_I2CDevice']]],
-  ['endtransaction',['endTransaction',['../class_adafruit___s_p_i_device.html#a65b517b7372df56322b807a08d067f8b',1,'Adafruit_SPIDevice']]]
+  ['endtransaction',['endTransaction',['../class_adafruit___s_p_i_device.html#a65b517b7372df56322b807a08d067f8b',1,'Adafruit_SPIDevice']]],
+  ['endtransactionwithdeassertingcs',['endTransactionWithDeassertingCS',['../class_adafruit___s_p_i_device.html#ad9e42c259682a03e76afd33c66af9317',1,'Adafruit_SPIDevice']]]
 ];
